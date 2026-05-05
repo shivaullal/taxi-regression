244c6e96a249bbbb12fe3298ec6f7f79b3387022
shivaullal <22k46.shiva@sjec.ac.in>
2026-05-05 12:23:27 +0530

    seven commit

--- supabase_config.py
+++ supabase_config.py
@@ -1,6 +1,6 @@
-from supabase import create_client
-url="https://apdtronjuennvhsigxyn.supabase.co"
-key="sb_publishable_2IDFWTmpEpk413F2069FGQ_IiS7XYFo"
+# from supabase import create_client
+# url="https://apdtronjuennvhsigxyn.supabase.co"
+# key="sb_publishable_2IDFWTmpEpk413F2069FGQ_IiS7XYFo"
 
 
-supabase=create_client(url,key)
+# supabase=create_client(url,key)
